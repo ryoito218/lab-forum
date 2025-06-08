c6048cc6a6995003d5f618b449054dd8b49ef899
ryoito218 <ryoito218@gmail.com>
2025-06-08 23:12:16 +0900

    feature: add create_post endpoint and PostCreate schema

--- backend/app/routers/posts.py
+++ backend/app/routers/posts.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import models
 from app.database import SessionLocal
+from app.schemas import PostCreate
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
 
@@ -16,3 +17,16 @@ def get_db():
 def read_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
+
+@router.post("/")
+def create_post(post_data: PostCreate, db: Session = Depends(get_db)):
+    db_post = models.Post(
+        title=post_data.title,
+        content=post_data.content,
+        category_id=post_data.category_id,
+        user_id=1
+    )
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
