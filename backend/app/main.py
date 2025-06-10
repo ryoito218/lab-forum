db038a5792e8c8c9754638981bdd02036f03f6f6
ryoito218 <ryoito218@gmail.com>
2025-06-10 13:12:43 +0900

    Feature: Add auth router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .database import engine
 from . import models
-from app.routers import posts, categories, users, tags
+from app.routers import posts, categories, users, tags, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -11,3 +11,4 @@ app.include_router(posts.router)
 app.include_router(categories.router)
 app.include_router(users.router)
 app.include_router(tags.router)
+app.include_router(auth.router)
