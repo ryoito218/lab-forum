de92eb9b87976f4768ed4785fd30cd5dbe9a7966
ryoito218 <ryoito218@gmail.com>
2025-07-05 18:06:41 +0900

    feature: add SimpleMarkdownEditor component and integrate create-post page

---
+++ frontend/src/components/SimpleMarkdownEditor.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import React, { useState } from 'react';
+import dynamic from 'next/dynamic';
+import remarkGfm from 'remark-gfm';
+
+const ReactMarkdown = dynamic(() => import('react-markdown'), { ssr: false });
+
+export interface SimpleMarkdownEditorProps {
+  markdown: string;
+  setMarkdown: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const SimpleMarkdownEditor: React.FC<SimpleMarkdownEditorProps> = ({
+  markdown,
+  setMarkdown,
+}) => {
+
+  return (
+    <div className='flex h-full gap-4 p-4'>
+      <textarea
+        className='w-1/2 h-full border rounded p-2 focus:outline-none focus:ring'
+        placeholder='ここに Markdown を入力'
+        value={markdown}
+        onChange={e => setMarkdown(e.target.value)}
+      />
+
+      <div className='w-1/2 h-full overflow-auto border rounded p-4 bg-white prose'>      
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+          {markdown}
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+
+export default SimpleMarkdownEditor;
